7c0e7a306b65cd45f4e9afe552d70d38e0405935
fahadalzubaidi <s202271360@kfupm.edu.sa>
2026-04-15 11:49:41 +0300

    Completed TODO1&2

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 
 // import dotenv and load environment variables from .env
+import dotenv from "dotenv";
+dotenv.config();
 
 
 import { connectDB } from "./db.js";
@@ -10,7 +12,7 @@ import { Song } from "./models/song.model.js";
 const app = express();
 const PORT = process.env.PORT || 5174;
 
-app.use(cors());              
+app.use(cors());
 app.use(express.json());
 
 await connectDB(process.env.MONGO_URL);
